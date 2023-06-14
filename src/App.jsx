2d0041d2939d46567fc21a76fb2b1aec25d742ea
Quaselz <paul.geppert94@freenet.de>
2023-06-14 16:24:03 +0200

    setup gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import MovieDetails from './pages/MovieDetails'
