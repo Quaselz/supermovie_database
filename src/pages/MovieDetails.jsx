3165ab5bcbe80a4994f4ac6c98d3f46151a8bb3b
Quaselz <paul.geppert94@freenet.de>
2023-06-14 09:56:12 +0200

    filling MovieDetailsPage

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,13 +1,27 @@
+import { useParams } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import Nav from "../components/Nav/Nav";
+import movies from "../data/moviedata";
 
 const MovieDetails = () => {
+    const params = useParams()
+    const title = params.title
+    const movie = movies.filter((movie) => movie.title === title)
+    console.log(movie[0]);
     return ( 
         <>
             <Nav />
             <main>
                 <section>
-                    <h1>MovieDetails</h1>
+                    <h1>Details about {movie[0].title}</h1>
+                    <p>{movie[0].year}</p>
+                    <p>{movie[0].director}</p>
+                    <p>{movie[0].duration}</p>
+                    <p>{movie[0].rate}</p>
+                    <ul>
+                         {movie[0].genre.map((item, i) => <li key={i}>{item}</li>)}
+                    </ul>
+                    
                 </section>
             </main>
             <Footer />
